2dfb1edba603bd4fc0936ba6b4c75a1b011de7f2
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-11 21:13:15 +0000

    + for loop in a component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  let results = ['4th', '3rd','2nd'];
+  let wins = [23, 23, 26];
   return (
     <div className="App">
       <div className='matches'>
@@ -19,6 +21,7 @@ function App() {
       <AddAttribute title='Dele' position='Midfield' score='6'  />
       <AddAttribute title='Harry' position='Forward' score='11' />
       </div>
+      <Tottenham results={results} wins={wins} />
     </div>
   );
 }
@@ -42,6 +45,24 @@ function DirectAttribute({team, date, time}) {
       <h1>Tottenham Hotspur vs {team}</h1>
       <h2>{date}</h2>
       <h5>{time}</h5>
+    </div>
+  )
+}
+
+function Tottenham ({results, wins}){
+
+  let [fourth,third, second] = results;
+  let totalWins = 0
+  for (let i = 0; i < wins.length; i++) {
+    totalWins += wins[i];
+  }
+  return (
+    <div className='results'>
+      <h4>2018/19 result : {fourth}, Wins : {wins[0]} </h4>
+      <h4>2017/18 result : {third}, Wins : {wins[1]} </h4>
+      <h4> 2016/17 result : {second}, Wins : {wins[2]} </h4>
+      {/* <h3> Total : {wins[0] + wins[1]+ wins[2]}</h3> */}
+      <h3>Total Wins : {totalWins}</h3>
     </div>
   )
 }
