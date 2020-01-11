2e60b8f9ddba2d9ccfcd651c17ecb1a44a45a86e
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-11 20:06:27 +0000

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+// import Spurs from './spurs.jpg';
+// import Liverpool from './liverpool.jpg';
+// import Watford from './watford.jpg';
+// import Sonny from './sonny.jpg';
+// import Dele from './dele.jpg';
+// import Harry from './harry.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className='matches'>
+      <DirectAttribute team='Liverpool' date='Saturday 11 January' time ='17:30'/>
+      <DirectAttribute team='Watford' date='Saturday 18 January' time ='12:30'/>
+      </div>
+      <div className='players'>
+      <AddAttribute title='Sonny' position='Forward' score='5'  />
+      <AddAttribute title='Dele' position='Midfield' score='6'  />
+      <AddAttribute title='Harry' position='Forward' score='11' />
+      </div>
     </div>
   );
 }
-/* git remote -v */
+
+function AddAttribute(props) {
+  return (
+    <div className='player'>
+      <h1>{props.title}</h1>
+      <h3>Position : {props.position}</h3>
+      <div>
+        <div>Season Goals : {props.score}</div>
+      </div>
+
+    </div>
+  );
+}
+
+function DirectAttribute({team, date, time}) {
+  return(
+    <div className='match'>
+      <h1>Tottenham Hotspur vs {team}</h1>
+      <h2>{date}</h2>
+      <h5>{time}</h5>
+    </div>
+  )
+}
+
+
 export default App;
